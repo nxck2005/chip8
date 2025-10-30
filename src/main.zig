ed61be685966bcce1e3a1610e135cd0d8143084c
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-10-30 20:54:10 +0530

    add sdl window logic and seperate emu logic

--- src/main.zig
+++ src/main.zig
@@ -13,5 +13,38 @@ pub fn init() !void {}
 pub fn deinit() void {}
 
 pub fn main() !void {
-    std.debug.print("Hit init\n", .{});
+    c.SDL_SetMainReady();
+    if (c.SDL_Init(c.SDL_INIT_VIDEO) != true) {
+        std.log.err("panic : SDL init failed: {s}\n", .{c.SDL_GetError()});
+        return;
+    }
+    defer c.SDL_Quit();
+
+    // create window
+    const window = c.SDL_CreateWindow(
+        "CHIP-8 Emulator",
+        640,
+        320,
+        c.SDL_WINDOW_RESIZABLE,
+    ) orelse {
+        std.log.err("panic : failed to create window: {s}\n", .{c.SDL_GetError()});
+        return;
+    };
+    defer c.SDL_DestroyWindow(window);
+
+    std.log.info("emulator window running; close to exit\n", .{});
+
+    // boilerplate loop
+    var running = true;
+    var event: c.SDL_Event = undefined;
+
+    while (running) {
+        _ = c.SDL_WaitEvent(&event);
+        switch (event.type) {
+            c.SDL_EVENT_QUIT => {
+                running = false;
+            },
+            else => {},
+        }
+    }
 }
