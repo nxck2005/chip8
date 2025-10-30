75d825ee821b9fdd301910ba071d1658e8b96c3d
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-10-30 19:04:14 +0530

    shift SDL logic to main

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,17 @@
 const std = @import("std");
 const chip8 = @import("root");
+const c = @cImport({
+    @cDefine("SDL_DISABLE_OLD_NAMES", {});
+    @cDefine("SDL_MAIN_HANDLED", {});
+    @cInclude("SDL3/SDL.h");
+    @cInclude("SDL3/SDL_main.h");
+});
+const process = std.process;
+
+pub fn init() !void {}
+
+pub fn deinit() void {}
 
 pub fn main() !void {
-    std.debug.print("", .{});
+    std.debug.print("Hit init\n", .{});
 }
--- src/root.zig
+++ src/root.zig
@@ -1,14 +1,3 @@
 const std = @import("std");
-const c = @cImport({
-    @cDefine("SDL_DISABLE_OLD_NAMES", {});
-    @cDefine("SDL_MAIN_HANDLED", {});
-    @cInclude("SDL3/SDL.h");
-    @cInclude("SDL3/SDL_main.h");
-});
-const process = std.process;
-
-pub fn init() !void {}
-
-pub fn deinit() void {}
 
 pub fn main() !void {}
